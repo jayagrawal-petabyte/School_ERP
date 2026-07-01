8e8b74058ff1d4ccf7f1c8636da61d4bb4200992
Nainesh Gorle <gorlenainesh@gmail.com>
2026-07-01 20:47:49 +0530

    Delete frontend directory

--- frontend/src/assignments/index.tsx
+++
@@ -1,4 +0,0 @@
-export { AssignmentCard } from './components/AssignmentCard';
-export { CreateAssignment } from './pages/CreateAssignment';
-export { StudentDashboard } from './pages/StudentDashboard';
-export { TeacherDashboard } from './pages/TeacherDashboard';
