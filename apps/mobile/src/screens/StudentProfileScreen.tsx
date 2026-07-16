edf4da8130ac781d6e9bbc3eaa48de69d4186060
DnN04 <dnofficial2005@gmail.com>
2026-07-16 14:40:13 +0530

    Fix duplicate SafeAreaView import syntax error in StudentProfileScreen

--- apps/mobile/src/screens/StudentProfileScreen.tsx
+++ apps/mobile/src/screens/StudentProfileScreen.tsx
@@ -6,7 +6,6 @@ import {
   StyleSheet,
   TouchableOpacity,
   ScrollView,
-  SafeAreaView,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { RouteProp } from '@react-navigation/native';
